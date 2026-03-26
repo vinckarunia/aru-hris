e2870f49ed5e18a6dd6984bea7402a0cdb4ed6bd
vinckarunia <vinckarunia@gmail.com>
2026-03-26 11:33:54 +0700

    chore: modify ssr for ziggy issue

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -3,7 +3,7 @@ import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import ReactDOMServer from 'react-dom/server';
 import { RouteName } from 'ziggy-js';
-import { route } from '../../vendor/tightenco/ziggy';
+import { route } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
